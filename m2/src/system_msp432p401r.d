a8da64eb368a63a434048410e6054e24ef1720b0
Burak Orpak <burak@oel.com.tr>
2020-05-13 14:29:35 +0300

    Creating Coursera Assignment Repository.

---
+++ m2/src/system_msp432p401r.d
@@ -0,0 +1,17 @@
+system_msp432p401r.o: system_msp432p401r.c /usr/include/stdc-predef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/8/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ ../include/msp432/msp432p401r.h ../include/msp432/msp_compatibility.h \
+ ../include/CMSIS/core_cm4.h ../include/CMSIS/core_cmInstr.h \
+ ../include/CMSIS/cmsis_gcc.h ../include/CMSIS/core_cmFunc.h \
+ ../include/CMSIS/core_cmSimd.h ../include/msp432/system_msp432p401r.h
